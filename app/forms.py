d65b2354feeac7d5c95f5ec4a8378055a4b3b6c8
olakayCoder1 <programmerolakay@gmail.com>
2022-09-05 15:47:31 +0100

    account update added

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField , EmailField  , PasswordField, SubmitField , ValidationError
 from wtforms.validators import  DataRequired , EqualTo , Length ,Email
+from flask_login import login_user , logout_user , login_required , current_user
+
 
 from .models import User
 
@@ -24,10 +26,37 @@ class RegisterForm(FlaskForm):
     submit = SubmitField('Sign-Up')
 #I NEED TO INSTALL EMAIL email_validation for EMAIL VALIDATION SUPPORT
 #I NEED TO INSTALL EMAIL email_validation for EMAIL VALIDATION SUPPORT
-#I NEED TO INSTALL EMAIL email_validation for EMAIL VALIDATION SUPPORT
+#I NEED TO INSTALL EMAIL email_validation for EMAIL VALIDATION SUPPORT   check_password
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
 
+
+class UserForm(FlaskForm):
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user and user.username != current_user.username :
+            raise ValidationError('Username already exist, try a different username')
+        
+    def validate_email(self, email_to_check):
+        user = User.query.filter_by(email=email_to_check.data).first()
+        if user and user.email != current_user.email :
+            raise ValidationError('Email already exist, try a different email')
+    username = StringField('Username', validators=[DataRequired(), Length(min=3)])
+    email = EmailField('Your Email', validators=[DataRequired(), Email()])
+
+    submit = SubmitField('Update')
+
+class PasswordChangeForm(FlaskForm):
+
+    def validate_old_password(self, old_password):
+        user = User.query.filter_by(username = current_user.username ).first()
+        if user and user.check_password(old_password):
+            return True
+        raise ValidationError('Old password did not match')
+    old_password = PasswordField('Current password' , validators=[DataRequired()])
+    new_password = PasswordField('Current password' , validators=[DataRequired(), Length(min=3)])
+    confirm_password = PasswordField('Current password' , validators=[EqualTo('password1')])
+    submit = SubmitField('Update password')
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
-from flask import flash , get_flashed_messages , render_template , request , redirect , url_for
+from flask import flash , get_flashed_messages , render_template , request , redirect , url_for , session
 from app import app , db , login_manager
-from app.forms import RegisterForm , LoginForm
+from app.forms import RegisterForm , LoginForm , UserForm
 from app.models import User , Task
 from flask_login import login_user , logout_user , login_required , current_user
 
@@ -64,12 +64,15 @@ def logout_page():
 
 
 @app.route("/home", methods=['GET','POST'])
+@login_required
 def home_page():
+    user_id = current_user.id
     if request.method == 'POST':
         description = request.form['description']
-        print(description)
-    print(current_user.id)
-    tasks = Task.query.all()
+        new_task = Task(description=description, creator=user_id)
+        db.session.add(new_task)
+        db.session.commit() 
+    tasks = Task.query.filter_by(creator=user_id)
     return render_template('home.html', tasks=tasks )
 
 
@@ -82,8 +85,45 @@ def delete_task_page(id):
 
 
   
-@app.route("/about")
-def about_page():
-    return render_template('about.html')
+@app.route("/account" , methods=['GET','POST'])
+def account_page():
+    form = UserForm()
+    print('herere 3')
+    if form.validate_on_submit():
+        print('herere')
+        user = User.query.get_or_404(current_user.id)
+        user.username = request.form['username']
+        user.email = request.form['email']
+        db.session.add(user)
+        db.session.commit()
+
+    elif form.errors != {} :
+        for err , message in form.errors.items():
+            flash(f"There was an error updating account : { message }", category='danger')
+    form.username.data = current_user.username
+    form.email.data = current_user.email
+    return render_template('about.html', form=form)
+
+
+@app.route("/password-reset", methods=['GET','POST'])
+@login_required
+def reset_password():
+    old_password = request.form['old_password']
+    new_password = request.form['new_password']
+    confirm_password = request.form['confirm_password']
+    user = User.query.filter_by(username = current_user.username ).first()
+    if user and user.check_password(old_password):
+        if new_password == confirm_password :
+            user.password = confirm_password
+            db.session.commit()
+            logout_user()
+            flash(f'You are to re-log in as password updated successfully', category='success')
+            return redirect(url_for('login_page')) 
+        else:
+            flash(f"There was an error updating account : { 'Confirm password did not match new password' }", category='danger')
+    else:
+        flash(f"There was an error updating account : {'Current password is not current' }", category='danger')
+    
+    return redirect(url_for('account_page'))
 
 
